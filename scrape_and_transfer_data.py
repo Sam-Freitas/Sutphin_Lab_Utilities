4e44c4347719646e622f0bb245d5af62a576705d
Sam Freitas <60946332+Sam-Freitas@users.noreply.github.com>
2021-10-14 13:17:56 -0700

    python script update

---
+++ scrape_and_transfer_data.py
@@ -0,0 +1,42 @@
+import os
+import glob
+import numpy as np
+import shutil
+from pathlib import Path
+from distutils.dir_util import copy_tree
+
+PATH_TO_WW = os.getcwd()
+PATH_TO_SUTPHIN = "/Volumes/Sutphin server/Projects/Worm Paparazzi/Data"
+EXT = "*.csv"
+all_csv_files = [file
+                 for path, subdir, files in os.walk(PATH_TO_WW)
+                 for file in glob.glob(os.path.join(path, EXT))]
+
+all_csv_files2 = [ x for x in all_csv_files if "division" not in x ]
+
+for i,this_filepath in enumerate(all_csv_files2):
+
+    file_name = Path(this_filepath).stem
+
+    this_dir = os.path.dirname(this_filepath)
+
+    activity_path = os.path.join(this_dir,"activity_groupings")
+
+    new_dir_path = os.path.join(PATH_TO_SUTPHIN,file_name)
+
+    new_activity_path = os.path.join(PATH_TO_SUTPHIN,file_name,"activity_groupings")
+
+    new_path = os.path.join(new_dir_path,file_name + ".csv")
+
+    if not os.path.exists(new_dir_path):
+        os.mkdir(new_dir_path)
+
+    shutil.copy(this_filepath,new_path)
+
+    print(file_name)
+
+    try: 
+        copy_tree(activity_path, new_activity_path)
+    except:
+        print(file_name, " Activity not found")
+
