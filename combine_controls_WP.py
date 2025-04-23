56db24a0fc80584f1dcee658e79d104ced84c3a0
Samuel "Sam" Freitas <samfreitas@email.arizona.edu>
2025-04-23 09:42:14 -0700

    Update combine_controls_WP.py

--- combine_controls_WP.py
+++ combine_controls_WP.py
@@ -50,34 +50,50 @@ if __name__ == "__main__":
     groupnames = list(processed_data_LUT["Groupname"])
     compounds = list(processed_data_LUT['Compound [XmM Name]'])
     bacterial_strains = list(processed_data_LUT["Bacterial Strains"])
+    egg_dates = list(processed_data_LUT["egg date"])
+    N = list(processed_data_LUT["N"])
+    NC = list(processed_data_LUT["NC"])
 
+    # 3 years ago today
+    cutoff_date = datetime.datetime.today() - datetime.timedelta(days=365.25*3)
+    format_string = "%Y-%m-%d"  # Y: year, m: month, d: day
+    # this_date = datetime.datetime.strptime(egg_date[0],format_string)
 
-    exclude_list = ['(rnai)','gls','0ug/ml cholesterol','gr','daf','skn','pmk','sun01','100','200','etoh','op50']
+    exclude_list = ['(rnai)','gls','0ug/ml cholesterol','gr','daf','skn','pmk',
+        'sun01','100','200','etoh','op50','jt10800','+','cop677','acox-1.2','15ug','20ug','25ug','0.5ug','soaked']
 
     controls_idxs = []
     controls_names = []
     controls_compounds = []
     controls_bacteria = []
+    controls_N = []
     for i in range(len(groupnames)):
         groupnames[i] = groupnames[i].lower()
         bacterial_strains[i] = bacterial_strains[i].lower()
-        if ('control' in groupnames[i]) or ('0m' in groupnames[i][0:2]) or ('0u' in groupnames[i][0:2]):
+        compounds[i] = str(compounds[i]).lower()
+        if ('control' in groupnames[i]) or ('0m' in groupnames[i][0:2]) or ('0u' in groupnames[i][0:2]) or ('5ug/ml cholesterol' in compounds[i]):
 
             continue_flag = True
             for this_exclude in exclude_list:
                 if (this_exclude in groupnames[i]) or (this_exclude in bacterial_strains[i]):
                     continue_flag = False
                     break
+
+            this_egg_datetime = datetime.datetime.strptime(egg_dates[i],format_string)
+
+            if this_egg_datetime < cutoff_date:
+                continue_flag = False
             
             if continue_flag:
                 controls_idxs.append(i)
                 controls_names.append(groupnames[i])
                 controls_compounds.append(compounds[i])
                 controls_bacteria.append(bacterial_strains[i])
+                controls_N.append(int(N[i]) - int(NC[i]))
    
     unique_control_names = unique_list(controls_names)
 
     for this_name in unique_control_names:
         print(this_name)
 
-    print('asdfasdf')
+print('EOF')
