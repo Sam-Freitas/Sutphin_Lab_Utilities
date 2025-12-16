824da6b145616a9643b7893434a29d84a67f2f35
Samuel "Sam" Freitas <samfreitas@email.arizona.edu>
2025-12-16 10:47:13 -0700

    Update cleanup_3_find_days_to_remove.py

--- server_functions/server_storage_cleanups/cleanup_3_find_days_to_remove.py
+++ server_functions/server_storage_cleanups/cleanup_3_find_days_to_remove.py
@@ -41,7 +41,7 @@ def get_subfolders(given_folder):
     subfolders = [ f.path for f in os.scandir(given_folder) if f.is_dir() ]
     return subfolders 
 
-def get_size_of_folder(root_directory):
+def get_size_of_folder(root_directory,decimals = 2):
 
     # give this a path not a Path object
     root_directory = Path(root_directory)
@@ -49,7 +49,7 @@ def get_size_of_folder(root_directory):
 
     out = folder_size/(2**30) # returns in GB
 
-    return round(folder_size/(2**30),2)
+    return round(folder_size/(2**30),decimals)
 
 def get_all_names_of_subfolders(given_folder):
     subfolders = natsorted(get_subfolders(given_folder))
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
     output_path = os.path.dirname(os.path.abspath(__file__))
 
-    df_column_names = ['days to remove','paths to remove']
+    df_column_names = ['days to remove','paths to remove','space savings']
 
     df = pd.read_csv(os.path.join(output_path,'cleanup_server2.csv'),index_col=False)
     print('FOUND COLUMNS:\n', list(df.columns))
@@ -105,17 +105,38 @@ if __name__ == '__main__':
 
             delta = end_date - start_date   # returns timedelta
 
-            day_list = []
+            day_list_to_remove = []
             for j in range(delta.days + 1):
                 day = start_date + datetime.timedelta(days=j)
-                day_list.append(day.strftime(format_string))
+                day_list_to_remove.append(day.strftime(format_string))
 
-            df.iat[i,df.columns.get_loc('days to remove')] = day_list
+            df.iat[i,df.columns.get_loc('days to remove')] = day_list_to_remove
 
             ##### add functions here where it finds the 
             # associated paths from each of the experiment plates
             # add appends them into a large list 
             # that would be read later for deletions 
+
+            unique_names, subfolders, all_data_folders = get_all_names_of_subfolders(df.loc[i]['path'])
+
+            all_data_folders_flattened = flatten_list(all_data_folders)
+
+            temp = [False]*len(all_data_folders_flattened)
+            for j,this_subsubdata_folder in enumerate(all_data_folders_flattened):
+                for jj,this_day_list_to_remove in enumerate(day_list_to_remove):
+                    if this_day_list_to_remove in this_subsubdata_folder:
+                        # print(this_day_list_to_remove,this_subsubdata_folder)
+                        temp[j] = True
+                        break
+
+            array_of_subfolders = np.asarray(all_data_folders_flattened)[temp]
+            list_of_subfolders = list(array_of_subfolders)
+
+            df.iat[i,df.columns.get_loc('paths to remove')] = list_of_subfolders
+
+            total_size = round(sum([get_size_of_folder(this_folder) for this_folder in list_of_subfolders]),2)
+
+            df.iat[i,df.columns.get_loc('space savings')] = total_size
 
             pass
 
