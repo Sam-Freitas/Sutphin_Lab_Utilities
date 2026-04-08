3c849b67baaa2f654394c01b0c9fa22865aa7118
Samuel "Sam" Freitas <samfreitas@email.arizona.edu>
2026-04-08 13:30:21 -0700

    Update read_usage.py

--- read_usage.py
+++ read_usage.py
@@ -3,4 +3,5 @@ import sys,os,glob
 from pandas import read_csv
 
 df = read_csv('system_metrics.csv')
-print('Max ram usage: ', max(df['ram_percent']))
+print('Max ram usage:    ', round(max(df['ram_used'])*(1e-9),2), '(GiB)')
+print('Max ram percent:  ', max(df['ram_percent']), '%')
