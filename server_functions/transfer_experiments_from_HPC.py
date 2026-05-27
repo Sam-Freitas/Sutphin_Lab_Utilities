0974b8a9bb67b3188b52c265610693870c737326
Samuel "Sam" Freitas <samfreitas@email.arizona.edu>
2026-05-27 16:03:07 -0700

    Create transfer_experiments_from_HPC.py

---
+++ server_functions/transfer_experiments_from_HPC.py
@@ -0,0 +1,89 @@
+import os,sys,glob,time,re, tempfile
+from natsort import natsorted
+import paramiko
+
+# this is the path to the txt file containing all the information
+ssh_details_path = os.path.normpath(r'server_functions\server_details.txt')
+
+# this is to sanitize the ssh outputs
+ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+
+def run_command(cmd, prompt_regex=r'[$#] ', timeout=5, verbose=True,wait_time = 2, timeout2 = 60):
+    print('SENDING:' + cmd)
+    shell.send(cmd + "\n")
+    buffer = ""
+    start = time.time()
+
+    if timeout == -1:
+        time.sleep(1)
+        # Continuous read mode
+        # timeout2 = timeout2  # seconds of inactivity before stopping
+        last_data_time = time.time()
+        while True:
+            if shell.recv_ready():
+                data = shell.recv(4096).decode(errors="ignore")
+                buffer += data
+                last_data_time = time.time()
+                if verbose and data.strip():
+                    print(data, end="", flush=True)
+            else:
+                if time.time() - last_data_time > timeout2:
+                    if verbose:
+                        print(f"\n[No data for {timeout2}s, stopping read]\n")
+                    break
+            time.sleep(0.1)
+    else:
+            # sleep time to make sure commands are processed 
+        time.sleep(wait_time)
+        # Normal mode with timeout and prompt detection
+        while True:
+            if shell.recv_ready():
+                data = shell.recv(4096).decode(errors="ignore")
+                buffer += data
+                if verbose and data.strip():
+                    print(data, end="", flush=True)
+                if re.search(prompt_regex, buffer):
+                    break
+            if time.time() - start > timeout:
+                break
+            time.sleep(0.1)
+
+    return buffer
+
+ssh_details = {}
+with open(ssh_details_path) as f:
+    for line in f:
+       (key, val) = line.split(maxsplit=1)
+       ssh_details[key] = val.rstrip()
+del key, val
+
+print(ssh_details_path) 
+print(ssh_details)
+
+ssh_client = paramiko.SSHClient()
+ssh_client.load_system_host_keys()
+ssh_client.connect(ssh_details['NAS_path'], username=ssh_details['NAS_username'], password=ssh_details['NAS_password'])
+
+print('Conecting to ssh shell')
+shell = ssh_client.invoke_shell()
+time.sleep(2)  # wait for command execution
+response = run_command('')
+time.sleep(2)
+
+response = run_command('cd /')
+response = run_command('pwd')
+response = run_command('cd ' + ssh_details['cd_path_for_Data'])
+response = run_command('pwd')
+
+# response = run_command('rsync -avzh --update --info=progress2 '+ ssh_details['rsync_get_path'] + ' .')
+response = run_command('rsync -avzh --update --info=progress2 samfreitas@filexfer.hpc.arizona.edu:/groups/sutphin/test_Data .')
+time.sleep(1)
+response = run_command(ssh_details['HPC_password'],timeout=-1, timeout2=15)
+time.sleep(1)
+response = run_command('1',timeout=-1, timeout2=60)
+time.sleep(1)
+
+ssh_client.close()
+
+
+print('EOF')
